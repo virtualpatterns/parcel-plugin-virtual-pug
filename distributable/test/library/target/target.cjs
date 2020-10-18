51a02c4ccbe866c3428c3909c70cf3d475096ddd
virtualpatterns <code.virtualpatterns@gmail.com>
2020-10-17 23:55:01 -0400

    post-test

--- distributable/test/library/target/target.cjs
+++ distributable/test/library/target/target.cjs
@@ -135,71 +135,17 @@ var _vtext = _interopRequireDefault(require("virtual-dom/vnode/vtext.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// Powered by @virtualpatterns/mablung-virtual-pug v0.0.1-12
+// FilePath = 'node_modules/@virtualpatterns/mablung-virtual-pug/distributable-commonjs/library/transform.cjs'
 const ConvertToVirtualNode = (0, _htmlToVdom.default)({
   VNode: _vnode.default,
   VText: _vtext.default
 });
 
 function __getNode(__local = {}, __option = {}) {
-  // Powered by @virtualpatterns/mablung-virtual-pug v0.0.1-11
-  // FilePath = 'node_modules/@virtualpatterns/mablung-virtual-pug/distributable-commonjs/library/transform.cjs'
   const {
     name
   } = __local;
-
-  function __forEach(value, fn) {
-    if (Array.isArray(value)) {
-      value.forEach(fn);
-      return value.length;
-    } else {
-      let entry = Object.entries(value);
-      entry.forEach(([name, value]) => fn(value, name));
-      return entry.length;
-    }
-  }
-
-  function __addAndAttribute(object, attributeNode) {
-    Object.entries(object).forEach(([name, value]) => __addAttribute(name, value, attributeNode)); // eslint-disable-line no-undef
-  }
-
-  function __getAttributeName(name) {
-    return name;
-  }
-
-  function __getAttributeValue(name, value, currentValue) {
-    if (typeof value === 'boolean') {
-      value = value ? name : false;
-    } else if (typeof value === 'string') {
-      value = currentValue ? `${currentValue} ${value}` : value;
-    } else if (Array.isArray(value)) {
-      value = currentValue ? `${currentValue} ${value.join(' ')}` : value.join(' ');
-    } else {
-      switch (name.toUpperCase()) {
-        case 'CLASS':
-          value = Object.keys(value).filter(key => value[key]).join(' ');
-          break;
-
-        case 'STYLE':
-          value = Object.keys(value).map(key => `${key}:${value[key]};`).join('');
-          break;
-      }
-    }
-
-    return value === '' ? undefined : value;
-  }
-
-  function __addAttribute(name, value, attributeNode) {
-    if (typeof value === 'boolean' && value === false) {// do nothing
-    } else {
-      name = __getAttributeName(name); // eslint-disable-line no-undef
-
-      value = __getAttributeValue(name, value, attributeNode[name]); // eslint-disable-line no-undef
-
-      if (value !== undefined) {
-        attributeNode[name] = value;
-      }
-    }
-  }
 
   function __getNodeName(name) {
     return name;
@@ -223,11 +169,11 @@ function __getNode(__local = {}, __option = {}) {
   }
 
   function __createNode(name, property, childNode, createNodeFn) {
-    name = __getNodeName(name); // eslint-disable-line no-undef
+    name = __getNodeName(name); //
 
-    property = __getNodeProperty(property); // eslint-disable-line no-undef
+    property = __getNodeProperty(property); //
 
-    childNode = __getChildNode(childNode); // eslint-disable-line no-undef
+    childNode = __getChildNode(childNode); //
 
     return createNodeFn(name, {
       attributes: property
@@ -235,8 +181,6 @@ function __getNode(__local = {}, __option = {}) {
   }
 
   function __getNode(__option = {}) {
-    // Powered by @virtualpatterns/mablung-virtual-pug v0.0.1-11
-    // FilePath = 'node_modules/@virtualpatterns/mablung-virtual-pug/distributable-commonjs/library/transform.cjs'
     const __node = [];
 
     __node.push(__createNode('p', {}, (() => {
@@ -269,8 +213,6 @@ function _default(__local = {}, __option = {
   createNode: _h.default,
   convertToNode: ConvertToVirtualNode
 }) {
-  // Powered by @virtualpatterns/mablung-virtual-pug v0.0.1-11
-  // FilePath = 'node_modules/@virtualpatterns/mablung-virtual-pug/distributable-commonjs/library/transform.cjs'
   return __getNode(__local, __option);
 }
 },{}],"source.js":[function(require,module,exports) {
